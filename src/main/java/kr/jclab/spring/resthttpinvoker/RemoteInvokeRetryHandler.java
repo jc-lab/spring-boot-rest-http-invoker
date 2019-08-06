27910c975ff9fa8262792d54e1cc627c5d2dcdd3
Jichan <development@jc-lab.net>
2019-08-06 13:47:06 +0900

    update

--- src/main/java/kr/jclab/spring/resthttpinvoker/RemoteInvokeRetryHandler.java
+++ src/main/java/kr/jclab/spring/resthttpinvoker/RemoteInvokeRetryHandler.java
@@ -1,5 +1,7 @@
 package kr.jclab.spring.resthttpinvoker;
 
+import java.io.IOException;
+
 public interface RemoteInvokeRetryHandler {
-    boolean checkRetry(int count);
+    boolean checkRetry(int count, IOException ioException, Throwable remoteThrowable);
 }
